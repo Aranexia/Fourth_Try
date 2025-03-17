9d688d40696863544269b55f52a0ba0387cf4611
Aranexia <aranex@duck.com>
2025-03-17 08:53:05 +0300

    Коммент?

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Account
+    {
+    
+        public string Name { get; set; }
+
+        public int Number { get; set; }
+
+        public double Summ { get; set; }
+    
+    }
+}
